a7823d3c7db0c506dfaca2d779f41185a0ef0784
dean.hsu <dean.hsu@exset.com>
2019-07-15 11:12:35 +0200

    check whether projects are specified in manifest.xml. Excluding non-manifest related prokects

--- repo_download_topics.py
+++ repo_download_topics.py
@@ -5,6 +5,7 @@ import sys
 import subprocess
 import json
 import re
+import mmap
 
 
 def is_child(patch, patch_list):
@@ -23,29 +24,40 @@ def is_child(patch, patch_list):
 
     return 0
 
-def get_patch_from_topic(topic_query_json, is_get_open_only=True):
-    patches = topic_query_json.splitlines()
-    patch_list = dict()  # index by project name
+def get_patch_from_topic(topic_query_json, manifest_file, is_get_open_only=True):
+	patches = topic_query_json.splitlines()
+	patch_list = dict()  # index by project name
 
-    for line in patches:
 	try:
-		patch = json.loads(line)
-		if "project" in patch and "currentPatchSet" in patch:
-			if is_get_open_only and patch["status"] != "NEW":
-				# skip merged and abandoned patches
-				continue
+		with open(manifest_file, 'r') as f:
+			xml_content = f.read()
+	except IOError as e:
+		print("manifest is not found: {}".format(manifest_file))
+		xml_content = None
 
-			if patch["project"] in patch_list:
-				number = patch_list[patch['project']]['number']
-				if is_child(patch, patch_list):
+	for line in patches:
+		try:
+			patch = json.loads(line)
+			if "project" in patch and "currentPatchSet" in patch:
+				if xml_content is not None and patch['project'] not in xml_content:
+					# skip this project because it is not specified in manifest.xml of repo
+					continue
+
+				if is_get_open_only and patch["status"] != "NEW":
+					# skip merged and abandoned patches
+					continue
+
+				if patch["project"] in patch_list:
+					number = patch_list[patch['project']]['number']
+					if is_child(patch, patch_list):
+						patch_list[patch["project"]] = patch
+				else:
 					patch_list[patch["project"]] = patch
-			else:
-				patch_list[patch["project"]] = patch
 
-	except:
-		pass
+		except:
+			pass
 
-    return patch_list
+	return patch_list
 
 def download_topics(args):
 	topic_list = re.split(',', args.topic)
@@ -53,7 +65,6 @@ def download_topics(args):
 
 	s = ""
 	for topic in topic_list:
-		topic = topic.strip()
 		command = ['ssh', '-p', str(args.port), args.server, args.remote, 'query', '--format=JSON', '--dependencies', '--current-patch-set', 'topic:' + topic]
 		print("querying '{}', '{}'".format(topic, " ".join(command)))
 		try:
@@ -68,7 +79,7 @@ def download_topics(args):
 			print("error code {}".format(grepexc.returncode))
 			sys.exit(1)
 
-	patches = get_patch_from_topic(s, not args.all_status)
+	patches = get_patch_from_topic(s, args.manifest ,not args.all_status)
 	if len(patches) == 0:
 		print("no open patches related to {}".format(args.topic))
 		sys.exit(0)
@@ -76,9 +87,8 @@ def download_topics(args):
 	command = ""
 	print("[projects related to {}]".format(args.topic))
 	for i, project in enumerate(patches, start=1):
-		patch = patches[project]
-		command = command + "repo download " + project + " " + patch['number'] + "/" + patch['currentPatchSet']['number'] + ";"
-		print("[{}]\t{}\t{} {}/{}\t{} ({})".format(i, patch['status'], project, patch['number'], patch['currentPatchSet']['number'], patch['branch'], patch['topic']))
+		command = command + "repo download " + project + " " + patches[project]['number'] + "/" + patches[project]['currentPatchSet']['number'] + ";"
+		print("[{}]\t{}\t{} {}/{}\t".format(i, patches[project]['status'], project, patches[project]['number'], patches[project]['currentPatchSet']['number']))
 
 	print("[download command]")
 	print(command)
@@ -90,14 +100,15 @@ def download_topics(args):
 
 
 def check_arg(args=None):
-    parser = argparse.ArgumentParser(description='repo download topics')
-    parser.add_argument('-t', '--topic', help='you can assign multiple topics by putting comma between them\nex: T-1234,T-5678', required=True)
-    parser.add_argument('-s', '--server', help='gerrit server', required=True)
-    parser.add_argument('-r', '--remote', help='git remote name', default="gerrit")
-    parser.add_argument('-p', '--port', help='ssh port', default=29418)
-    parser.add_argument('--download', help='download patches after querying', dest='download', action='store_true')
-    parser.add_argument('--all_status', help='also download abandoned and merged patches', dest='all_status', action='store_true')
-    return parser.parse_args(args)
+	parser = argparse.ArgumentParser(description='repo download topics')
+	parser.add_argument('-t', '--topic', help='you can assign multiple topics by putting comma between them\nex: T-1234,T-5678', required=True)
+	parser.add_argument('-s', '--server', help='gerrit server', required=True)
+	parser.add_argument('-r', '--remote', help='git remote name', default="gerrit")
+	parser.add_argument('-p', '--port', help='ssh port', default=29418)
+	parser.add_argument('-m', '--manifest', help='manifest xml of the (repo) project', default=".repo/manifest.xml")
+	parser.add_argument('--download', help='download patches after querying', dest='download', action='store_true')
+	parser.add_argument('--all_status', help='also download abandoned and merged patches', dest='all_status', action='store_true')
+	return parser.parse_args(args)
 
 if __name__ == '__main__':
     download_topics(check_arg(sys.argv[1:]))
